595bf07ff9f6b62340dd418f935febe696e5f2d7
JaumeUNI <jcasamitjana@campus.eug.es>
2025-04-19 19:23:36 +0200

    logic

--- tokenforge/src/types/solana.d.ts
+++ tokenforge/src/types/solana.d.ts
@@ -1,15 +1,1 @@
-interface PhantomProvider {
-    isPhantom: boolean;
-    isConnected: boolean;
-    publicKey: {
-      toString(): string;
-    };
-    connect(): Promise<{ publicKey: string }>;
-    disconnect(): Promise<void>;
-    signTransaction(transaction: any): Promise<any>;
-    signAllTransactions(transactions: any[]): Promise<any[]>;
-  }
-  
-  interface Window {
-    solana?: PhantomProvider;
-  }
+//TODO
